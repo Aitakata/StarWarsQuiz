488b1bb2012e8e80afe44d1520231f75a9ae49ca
Artur Issam Takata <Artur.takata@gmail.com>
2021-02-02 22:27:43 -0300

    versao ao final da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import Head from 'next/head';
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer/index.js';
-import GitHubCorner from '../src/components/GitHubCorner/index.js';
-import QuizBackground from '../src/components/QuizBackground/index.js';
-import QuizLogo from '../src/components/QuizLogo/index.js';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer/index';
+import GitHubCorner from '../src/components/GitHubCorner/index';
+import QuizBackground from '../src/components/QuizBackground/index';
+import QuizLogo from '../src/components/QuizLogo/index';
 
-const BackgroundImage = styled.div`
+/* const BackgroundImage = styled.div`
   background-image: url(${db.bg});
   flex: 1;
   background-size: cover;
   background-position: center;
-`
+`; */
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
@@ -24,25 +26,56 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name, setName);
+
   return (
     <QuizBackground backgroundImage={db.bg}>
-      <Head> 
-        <title>ArturQuiz - Modelo básico</title>
-      </Head>  
+      <Head>
+        <title>ArturQuiz</title>
+        <meta name="title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta name="description" content="Descriçao do ArturQuiz" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      </Head>
       <QuizContainer>
         <QuizLogo />
-
-      
         <Widget>
           <Widget.Header>
             <h1>Quiz Star Wars</h1>
           </Widget.Header>
-          <Widget.Content> 
+          <Widget.Content>
             <p>Que tal testar seus conhecimentos sobre a Saga Star Wars</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do React');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+
+                {name}
+              </button>
+            </form>
           </Widget.Content>
-        </Widget> 
+        </Widget>
         <Widget>
           <Widget.Header>
             <h1>Quiz da Galera</h1>
@@ -53,7 +86,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/aitakata"/>
+      <GitHubCorner projectUrl="https://github.com/aitakata" />
     </QuizBackground>
   );
 }
